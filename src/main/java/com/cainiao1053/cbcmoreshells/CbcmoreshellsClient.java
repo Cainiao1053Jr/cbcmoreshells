2542edffba385f246b06ba301fd6c4ce7d5ccc33
Cainiao1053 <zijians4@illinois.edu>
2026-04-21 19:35:01 -0500

    Updated

--- src/main/java/com/cainiao1053/cbcmoreshells/CbcmoreshellsClient.java
+++ src/main/java/com/cainiao1053/cbcmoreshells/CbcmoreshellsClient.java
@@ -6,23 +6,6 @@ import com.cainiao1053.cbcmoreshells.client.render.DynamicOutliner;
 public class CbcmoreshellsClient {
 
     public static final DynamicOutliner CLIENT_LERPED_OUTLINER = new DynamicOutliner();
-//	@SubscribeEvent
-//	public static void onClientSetup(FMLClientSetupEvent event) {
-//		event.enqueueWork(() -> {
-//			EntityType<?> cannon = ForgeRegistries.ENTITY_TYPES.getValue(
-//					new ResourceLocation("createbigcannons", "ap_shot"));
-//
-//			if (cannon != null) {
-//				RenderHandler.registerExtraLayer(
-//						cannon,
-//						new BillboardOverlay(
-//								new ResourceLocation(Cbcmoreshells.MODID, "textures/block/projectile/apbc_shot.png"),
-//								0.6f
-//						)
-//				);
-//			}
-//		});
-//	}
 
     public static void clientInit(){}
 
