0b63bfdf44cdf89d4e65aecb5495727c6269eb7e
Cainiao1053 <zijians4@illinois.edu>
2026-04-25 19:55:48 -0500

    Updated

--- src/main/java/com/cainiao1053/cbcmoreshells/cannons/projectile_rack/ProjectileRackBlockRenderer.java
+++ src/main/java/com/cainiao1053/cbcmoreshells/cannons/projectile_rack/ProjectileRackBlockRenderer.java
@@ -24,21 +24,21 @@ public class ProjectileRackBlockRenderer implements BlockEntityRenderer<Projecti
     public void render(ProjectileRackBlockEntity blockEntity, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
         Block loadedBlock = blockEntity.getBlockState().getBlock();
 
-//        if (loadedBlock instanceof ProjectileRackBlock) {
-//            BlockState loadedBlockState = blockEntity.cannonBehavior().block().state();
-//            BlockState blockState = blockEntity.getBlockState();
-//            Direction facing = (Direction) blockState.getValue(BlockStateProperties.FACING);
-//            VertexConsumer vcons = buffer.getBuffer(RenderType.solid());
-//            SuperByteBuffer c = CachedBuffers.block(loadedBlockState);
-//            //float rotationAngle = facing.getAxisDirection().getStep() == 1 ? 0.0F : 180.0F;
-//            c.rotateCentered(Axis.YN.rotationDegrees(0))
-//                    .translate(facing.step().mul(0.6f))
-//                    .light(light)
-//                    .forEntityRender()
-//                    .renderInto(ms, vcons);
-//            ms.pushPose();
-//            ms.popPose();
-//        }
+        if (loadedBlock instanceof ProjectileRackBlock) {
+            BlockState loadedBlockState = blockEntity.cannonBehavior().block().state();
+            BlockState blockState = blockEntity.getBlockState();
+            Direction facing = (Direction) blockState.getValue(BlockStateProperties.FACING);
+            VertexConsumer vcons = buffer.getBuffer(RenderType.solid());
+            SuperByteBuffer c = CachedBuffers.block(loadedBlockState);
+            //float rotationAngle = facing.getAxisDirection().getStep() == 1 ? 0.0F : 180.0F;
+            c.rotateCentered(Axis.YN.rotationDegrees(0))
+                    .translate(facing.step().mul(0.6f))
+                    .light(light)
+                    .forEntityRender()
+                    .renderInto(ms, vcons);
+            ms.pushPose();
+            ms.popPose();
+        }
 
     }
 }
